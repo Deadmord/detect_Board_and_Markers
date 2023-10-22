48ebc1d2252f448178d026f6393569d75e314eda
Nikolay Belokonenko <belokonenkons@gmail.com>
2023-10-22 21:04:17 +0300

    create some markers

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ namespace {
 
 const char* argvCreateMarker[] = {
         "program_name - create marker",   // Здесь "program_name" может быть любой строкой, она обычно является именем исполняемого файла
-        "markers/marker42.png",
+        "markers/marker50.png",
         "-d=10",
-        "-id=42",
+        "-id=50",
         "-ms=200",
         "-bb=1",
         "-si=true",
